37e3f122ba61951b4551d7ae9a47864e10566edb
Maksim Chartkou <maksim.chartkou@altoros.com>
2016-02-05 19:00:04 +0300

    a bit doc & rename

--- gulp-pipe.js
+++ gulp-pipe.js
@@ -2,25 +2,25 @@ module.exports = function(RED) {
 	var gulp = require('gulp');
   var fs = require('fs');
 
-  var _loaded_modules = {};
+  var _loaded_plugins = {};
 
   // load modules
-  var gulp_modules = [];
+  var plugin_name_arr = [];
   var files = fs.readdirSync('node_modules');
   for(var i=0; i<=files.length-1; i++){
       if(files[i].match(/^gulp\-/)){
-          load_gulp_module(files[i]);
+          load_gulp_plugin(files[i]);
       }
   }
 
-  function load_gulp_module(name){
+  function load_gulp_plugin(name){
 			RED.log.info('Loading gulp module: '+name );
-      _loaded_modules[name] = require(name);
-      gulp_modules.push(name);
+      _loaded_plugins[name] = require(name);
+      plugin_name_arr.push(name);
   }
 
   //console.log('gulp_modules', gulp_modules);
-	RED.comms.publish("gulp_pipe_list", gulp_modules, true);
+	RED.comms.publish("gulp_pipe_list", plugin_name_arr, true);
 
 	/**
 	 *
@@ -31,17 +31,17 @@ module.exports = function(RED) {
 
 		this.on('input', function(msg){
 
-			var module_name = config.module;
+			var plugin_name = config.plugin;
 			var arg1 = config.arg1;
 
 			
-			if( module_name && _loaded_modules[module_name] ){
-				var module = _loaded_modules[module_name];
+			if( plugin_name && _loaded_plugins[plugin_name] ){
+				var plugin = _loaded_plugins[plugin_name];
 				
-				var p = msg.payload.pipe( module(arg1) );
+				var p = msg.payload.pipe( plugin(arg1) );
 				node.send({payload:p});
 			}else{
-				RED.log.warn('gulp pipe: unknown module. it will be skipped');
+				RED.log.warn('gulp pipe: unknown plugin. it will be skipped');
 				node.send(msg);
 			}
 			
