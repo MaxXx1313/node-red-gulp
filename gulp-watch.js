5c175cc0d82ff61f18ac56ff6af92c333c0952d5
Maksim Chartkou <maksim.chartkou@altoros.com>
2016-02-05 18:28:26 +0300

    gulp watch

---
+++ gulp-watch.js
@@ -0,0 +1,21 @@
+module.exports = function(RED) {
+	var gulp = require('gulp');
+
+	RED.nodes.registerType("gulp watch", function (config) {
+		RED.nodes.createNode(this, config);
+		var node = this;
+
+		// https://github.com/gulpjs/gulp/blob/master/docs/API.md#gulpwatchglob--opts-tasks-or-gulpwatchglob--opts-cb
+		var globs = config.globs || '';
+		var options = config.options || {};
+
+		gulp.watch(globs, options, function(e){
+			console.log('File ' + e.path + ' was ' + e.type + ', running tasks...');
+			
+			node.send({	payload:{globs:globs, event:e}});
+
+		});
+	
+	});
+}
+
